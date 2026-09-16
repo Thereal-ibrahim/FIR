b49837ebec8ba6c28b45dbace5acda692c09493e
Thereal-ibrahim <ibrahim.sayed.abdelhannan@gmail.com>
2026-09-16 07:15:03 +0300

    This version probably has an issue with accumulation in lines 45-48

--- FIR.sv
+++ FIR.sv
@@ -1,71 +1,60 @@
 
 
-module FIR #(parameter int N = 16;) (
-logic signed [N-1:0] inp_sig, //16 bits (Q8.8)
+module FIR #(parameter int Width = 16, parameter int Taps = 31 ) // 
+ (
+input logic signed [Width-1:0] inp_sig, //16 bits (Q8.8)
 
+output logic signed [(2*Width)+ $clog2(Taps)+1:0]  out_sig, // 37 bits +sign bit
 
-logic signed [(2*N)+4:0]  out_sig, //36 bits +sign bit
-
-logic CLK, n_RST
+input logic CLK, n_RST
 
 );
 
 
 
 
-logic signed [N-1:0] D_wire [N-2:0]; //16bits
-logic signed out_sig_reg;
+logic signed [Width-1:0] D_wire [Taps-2:0]; //16bits each, 30 elements (Taps-1), 
+logic signed [(2*Width)+$clog2(Taps)+1:0] out_sig_reg;
 
-//
-//to be edited as input////
-//
 
-// Q1.15 coefficients: the integer values sum to 32768 (unity DC gain).
-
-parameter signed Coeff0 = 16'sh0180;
-parameter signed Coeff1 = 16'sh0200;
-parameter signed Coeff2 = 16'sh0380;
-parameter signed Coeff3 = 16'sh0600;
-parameter signed Coeff4 = 16'sh0900;
-parameter signed Coeff5 = 16'sh0C00;
-parameter signed Coeff6 = 16'sh0E80;
-parameter signed Coeff7 = 16'sh0F80;
-parameter signed Coeff8 = 16'sh0F80;
-parameter signed Coeff9 = 16'sh0E80;
-parameter signed Coeff10 = 16'sh0C00;
-parameter signed Coeff11 = 16'sh0900;
-parameter signed Coeff12 = 16'sh0600;
-parameter signed Coeff13 = 16'sh0380;
-parameter signed Coeff14 = 16'sh0200;
-parameter signed Coeff15 = 16'sh0180;
+// Q1.15 coefficients supplied for the 31-tap filter.
+parameter logic signed [Width-1:0] Coeff [0:Taps-1] = '{ //[0:Taps-1] to start with the first element of the array at index 0
+    16'sd41,    -16'sd23,   -16'sd11,    16'sd81,
+   -16'sd192,   16'sd323,  -16'sd416,   16'sd389,
+   -16'sd155,  -16'sd345,  16'sd1117, -16'sd2091,
+   16'sd3132, -16'sd4063,  16'sd4709, 16'sd27780,
+   16'sd4709, -16'sd4063,  16'sd3132, -16'sd2091,
+   16'sd1117,  -16'sd345,  -16'sd155,   16'sd389,
+   -16'sd416,   16'sd323,  -16'sd192,    16'sd81,
+   -16'sd11,    -16'sd23,    16'sd41
+};
 
 always @(posedge CLK or negedge n_RST) begin
-
-    D_wire[0] = inp_sig;
-    for (i=1; i<= N-2; i++) begin
-        D_wire[i] = D_wire[i-1];
+    if (!n_RST) begin
+        for (int i=0; i<= Taps-2; i++) begin
+            D_wire[i] <= 0;
+        end
+        out_sig_reg <= 0;
+    end
+    else begin
+    D_wire[0] <= inp_sig;
+    for (int i=1; i<= Taps-2; i++) begin
+        D_wire[i] <= D_wire[i-1];
     end
 
-    out_sig_reg = inp_sig * Coeff0 +
-                  D_wire[0] * Coeff1 +
-                  D_wire[1] * Coeff2 +
-                  D_wire[2] * Coeff3 +
-                  D_wire[3] * Coeff4 +
-                  D_wire[4] * Coeff5 +
-                  D_wire[5] * Coeff6 +
-                  D_wire[6] * Coeff7 +
-                  D_wire[7] * Coeff8 +
-                  D_wire[8] * Coeff9 +
-                  D_wire[9] * Coeff10 +
-                  D_wire[10] * Coeff11 +
-                  D_wire[11] * Coeff12 +
-                  D_wire[12] * Coeff13 +
-                  D_wire[13] * Coeff14 +
-                  D_wire[14] * Coeff15;
+    out_sig_reg <= inp_sig * Coeff[0];
+    for (int i = 1; i < Taps; i++) begin
+        out_sig_reg <= out_sig_reg + D_wire[i-1] * Coeff[i];
+    end
+
+
+
 
     
 end
 
+
+assign out_sig = out_sig_reg;
 
 
 endmodule
